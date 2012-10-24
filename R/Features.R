08f4a5577a0c9d0682e511b72cd4b6e380876911
Tim Yates <tyates@picr.man.ac.uk>
2012-10-24 12:37:52 +0100

    Added features call

---
+++ R/Features.R
@@ -0,0 +1,34 @@
+featuresByRegion = function( region, species,
+                             feature=c( 'gene', 'transcript', 'cds', 'exon',
+                                        'variation', 'somatic_variation',
+                                        'structural_variation',
+                                        'structural_somatic_variation',
+                                        'constrained', 'regulatory' ),
+                             db_type=c( NA, 'core', 'otherfeatures' ),
+                             logic_name=NULL,
+                             so_term=NULL,
+                             species_set=NULL ) {
+  db_type = match.arg( db_type )
+  feature = if( missing( feature ) ) 'gene' else match.arg( feature, several.ok=TRUE )
+  params = c()
+  if( !is.na( db_type ) ) params = c( params, .make.params( db_type=db_type ) )
+  if( !is.null( feature ) ) params = c( params, .make.params( feature=feature ) )
+  if( !is.null( logic_name ) ) params = c( params, .make.params( logic_name=logic_name ) )
+  if( !is.null( so_term ) ) params = c( params, .make.params( so_term=so_term ) )
+  if( !is.null( species_set ) ) params = c( params, .make.params( species_set=species_set ) )
+  munger = function( e ) {
+    # Rename to space for GRanges
+    if( 'seq_region_name' %in% names( e ) ) {
+      names( e )[ names( e ) == 'seq_region_name' ] = 'space'
+    }
+    # characterize strand
+    if( 'strand' %in% names( e ) ) {
+      e$strand = .strandString( e$strand )
+    }
+    # Strip NULL and make a data.frame
+    data.frame( e[ !sapply( e, is.null ) ] )
+  }
+  as( as( do.call( 'rbind.fill',
+    lapply( .load.and.parse( .Ensembl$feature, c( species=species, region=region ), params ), munger )
+  ), 'RangedData' ), 'GRanges' )
+}
