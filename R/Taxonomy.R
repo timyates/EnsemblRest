f890b16862f52b9e527cc4a9a04b5280bef4e552
Tim Yates <tyates@picr.man.ac.uk>
2013-04-18 15:14:57 +0100

    taxonomyClassification function and docs

--- R/Taxonomy.R
+++ R/Taxonomy.R
@@ -3,3 +3,7 @@ taxonomyId = function( id, simple=FALSE ) {
   if( simple ) params = c( params, .make.params( simple='1' ) )
   getRefClass( 'EnsTaxonomy' )$new( .load.and.parse( .Ensembl$taxonomy.id, c( id=id ), params ) )
 }
+
+taxonomyClassification = function( id ) {
+  lapply( .load.and.parse( .Ensembl$taxonomy.classification, c( id=id ), c() ), getRefClass( 'EnsTaxonomy' )$new )
+}
